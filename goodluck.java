eaf240bdfd4f158082adc509fe02e42cd327f909
kishore-shinde <69797765+kishore-shinde@users.noreply.github.com>
2021-01-24 16:52:22 +0530

    Update goodluck.java

--- goodluck.java
+++ goodluck.java
@@ -1,5 +1,5 @@
 class goodluck {
     public static void main(String[] args) {
-        System.out.println("Good, Luck!"); 
+        System.out.println("Good, Luck Updated for Build 3!"); 
     }
 }
