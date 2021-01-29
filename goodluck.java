1cbdbfcf8504c6e914289195b9f8d7f4e3b454b4
kishore-shinde <69797765+kishore-shinde@users.noreply.github.com>
2021-01-29 18:18:49 +0530

    Update goodluck.java

--- goodluck.java
+++ goodluck.java
@@ -1,5 +1,5 @@
 class goodluck {
     public static void main(String[] args) {
-        System.out.println("Good, Luck Updated for Build 4!"); 
+        System.out.println("Welcome to Project-Jenkins"); 
     }
 }
